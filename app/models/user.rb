1c1d79cd9afdd1ae7bc6253f1192d8f34fe6c9ae
ReadingPig <zyzpop830203@163.com>
2017-05-30 10:53:33 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :groups
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
   has_many :posts
 end
